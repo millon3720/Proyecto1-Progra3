c2be59c60df2e155cdea98c5432a3ff388a732fb
millon3720 <josed200096@gmail.com>
2023-02-18 18:20:51 -0600

    Parte 1

---
+++ JavaScript.js
@@ -0,0 +1,64 @@
+function Pregunta1()
+{
+  var Palabra="";
+  for (var i = 1; i < 6; i++) {
+    Palabra+=document.getElementById('Letra'+i).value;
+  }
+  if (Palabra.toLowerCase()=="viaje") {
+    alert("La Palabra Digitada Es Correcta");
+    return window.open("Pregunta2.html");
+  }
+  else {
+    alert("La Palabra Digitada Es Incorrecta");
+  }
+
+}
+function Pregunta2()
+{
+  var Palabra="";
+  for (var i = 1; i < 6; i++) {
+    Palabra+=document.getElementById('Letra'+i).value;
+  }
+  if (Palabra.toLowerCase()=="viaje") {
+    alert("La Palabra Digitada Es Correcta");
+  }
+  else {
+    alert("La Palabra Digitada Es Incorrecta");
+  }
+}function Pregunta3()
+{
+  var Palabra="";
+  for (var i = 1; i < 6; i++) {
+    Palabra+=document.getElementById('Letra'+i).value;
+  }
+  if (Palabra.toLowerCase()=="viaje") {
+    alert("La Palabra Digitada Es Correcta");
+  }
+  else {
+    alert("La Palabra Digitada Es Incorrecta");
+  }
+}function Pregunta4()
+{
+  var Palabra="";
+  for (var i = 1; i < 6; i++) {
+    Palabra+=document.getElementById('Letra'+i).value;
+  }
+  if (Palabra.toLowerCase()=="viaje") {
+    alert("La Palabra Digitada Es Correcta");
+  }
+  else {
+    alert("La Palabra Digitada Es Incorrecta");
+  }
+}function Pregunta5()
+{
+  var Palabra="";
+  for (var i = 1; i < 6; i++) {
+    Palabra+=document.getElementById('Letra'+i).value;
+  }
+  if (Palabra.toLowerCase()=="viaje") {
+    alert("La Palabra Digitada Es Correcta");
+  }
+  else {
+    alert("La Palabra Digitada Es Incorrecta");
+  }
+}
